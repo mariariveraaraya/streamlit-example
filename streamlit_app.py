52d2e71d77a89f52047177ff9b25905e10cd9c3e
Maria Rivera Araya <50934176+mariariveraaraya@users.noreply.github.com>
2023-11-23 12:57:21 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,10 @@ questions = {
 # Create the quiz app
 def quiz_app():
     st.title("Quiz App")
-    correct_answers = 0
-    for question, answers in questions.items():
-        st.subheader(question)
-        user_answer = st.radio("", list(answers.keys()))
-        if st.button("Submit"):
-            if answers[user_answer]:
-                st.success("Correct!")
-                correct_answers += 1
-            else:
-                st.error("Incorrect. Try again.")
-    if st.button("Calculate Grade"):
-        grade = (correct_answers / len(questions)) * 100
+    user_answers = {question: {option: st.checkbox(option, key=f"{question}_{option}") for option in answers.keys()} for question, answers in questions.items()}
+    if st.button("Submit") and all(any(answers.values()) for answers in user_answers.values()):
+        score = sum(all(user_answers[question][option] == correct for option, correct in answers.items()) for question, answers in questions.items())
+        grade = (score / len(questions)) * 100
         st.write(f"Your grade is {grade}%")
 
 # Run the app
